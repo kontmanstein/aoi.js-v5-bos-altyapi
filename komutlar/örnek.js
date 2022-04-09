6bb84f39f67db3b69f3cddacaad8d9f4073298b4
Kont Manstein <85468547+kontmanstein@users.noreply.github.com>
2022-04-09 06:55:34 +0300

    Create örnek.js

---
+++ komutlar/örnek.js
@@ -0,0 +1,17 @@
+module.exports = [{
+name:"örnek",
+aliases:["örnek1","örnek2","örnek3"],
+usage: "örnek",
+description: "Örnek Komut",
+category: "Örnek",
+code:`
+Bu bir örnektir <@$authorID>`
+},{ // bir komutda iki komut kullanma
+name:"örnek",
+aliases:["örnek1","örnek2","örnek3"],
+usage: "örnek",
+description: "Örnek Komut",
+category: "Örnek",
+code:`
+Bu bir örnektir <@$authorID>`
+}];
