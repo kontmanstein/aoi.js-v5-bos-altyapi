b8ee381f094c1d3aa68516552a6a2e4fea78be5e
Kont Manstein <85468547+kontmanstein@users.noreply.github.com>
2022-04-09 06:56:11 +0300

    Update durum.js

--- işleyici/durum.js
+++ işleyici/durum.js
@@ -1,8 +1,26 @@
-module.exports = (bot) => {
-    bot.status({
-        text: `$serverCount Sunucu | $allMembersCount Kişi | ${bot.prefix}yardım`,
-        type: "LISTENING",
-        status: "idle",
-        time: 15
-    });
-};
+/*
+ Status = {
+  {@text} : string | number,
+  {@type} : "PLAYING" | "playing" | "STREAMING" | "streaming" | "WATCHING" | "watching" | "LISTENING" | "listening",
+  {@time} : number,
+  {@status?} : "online" | "offline" | "idle" | "dnd",
+  {@url?} : string,
+  {@shardId?} : number | Array<number> 
+ }
+ returns Array<Status>
+*/
+
+module.exports = (bot) => [
+    {
+    text: `$serverCount Sunucu | $allMembersCount Kişi | ${bot.prefix}yardım`,
+    type: "LISTENING",
+    status: "idle",
+    time: 15
+    },
+    {
+    text : "Seni",
+    type : "WATCHING",
+    status: "idle",
+    time : 15
+    }
+];
