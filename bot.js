eca42c55c02e26648ae06c71a3034b976bc7d729
Kont Manstein <85468547+kontmanstein@users.noreply.github.com>
2022-04-09 06:54:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const fs = require('node:fs');
 
 const bot = new aoijs.Bot({
 token: process.env['token'],
-prefix: ["prefix1","prefix2","prefix3"],
+prefix: ["prefix1", "prefix2", "prefix3"],
 intents: "all",
 database: {
     type: "default",
@@ -14,6 +14,13 @@ database: {
     tables: ["main"],
     promisify: false
 },
+cache: {
+  users: 100,
+  messages: 10,
+  presences : 0,
+  guildEmojis : 0,
+  guildStickers : 0
+}
 });
 
 //Örnek Komut (ping)
@@ -23,36 +30,11 @@ code: `Pong! \`$pingms\``
 });
 
 //Komut Yükleyici
-
 const loader = new aoijs.LoadCommands(bot);
 (async () => {
 await loader.load(bot.cmd, "./komutlar/")
 })();
-
-loader.setColors({
-  walking: ["blink", "dim", "fgWhite"],
-  failedWalking: {
-    name: ["bright", "fgYellow", "underline"],
- 
-    text: ["bright", "fgRed"]
-  },
-  typeError: {
-    command: ["bright", "fgYellow"],
-    type: ["fgYellow"],
-    text: ["bright", "fgRed"]
-  },
-  failLoad: {
-    command: ["bright", "fgMagenta"],
-    type: ["fgRed"],
-    text: ["bright", "fgRed"],
-  },
-  loaded: {
-    command: ["bright", "fgCyan"],
-    type: ["bright", "fgBlue"],
-    text: ["bright", "fgGreen"]
-  },
- 
-});
+loader.setColors( loader.themes.default );
 
 //eventler
 const files = fs.readdirSync('./eventler').filter(file => file.endsWith('.js'))
@@ -61,6 +43,12 @@ require(`./eventler/${x}`)(bot)
 });
 
 //ayarlar
-require('./işleyici/durum')(bot);
-require('./işleyici/değişkenler')(bot);
+const durum = require('./işleyici/durum')(bot);
+const değişkenler = require('./işleyici/değişkenler');
 require('./işleyici/callbacks')(bot);
+
+//durum
+bot.status ( ...durum );
+
+//değişkenler
+bot.variables( ...değişkenler );
